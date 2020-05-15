a11c0ad8f867a4955e82e59915b71a6f06351c14
isocracy272 <crawl2720@hotmail.co.uk>
2020-05-15 22:10:00 +0100

    New Code

--- application.py
+++ application.py
@@ -1,1 +1,17 @@
-#AndtotheEdge
+import xlrd
+import xlwt
+
+#Start at the top of the logs
+logR= 1
+
+#match variables for 
+srcNetM = 0
+dstNetM = 0
+
+#Iterate through logs and find network
+while srcNetM and dstNetM:
+	logR = logR + 1
+
+
+
+#ToTheEdge
