d37cb26fdbff321ce45d17e29a7decd56bcb42db
isocracy272 <crawl2720@hotmail.co.uk>
2020-06-07 19:26:29 +0100

    More Code

--- application.py
+++ application.py
@@ -1,5 +1,29 @@
+#Imports
 import xlrd
 import xlwt
+
+#Functions
+
+#Function to resolve Magic Number based on mask
+def getMagicNumber(mask):
+	if mask = 30:
+		mn = 4
+	if mask = 29:
+		mn = 8
+	if mask = 28:
+		mn = 16
+	if mask = 27:
+		mn = 32
+	if mask = 26:
+		mn = 64
+	if mask = 25:
+		mn = 128
+	if mask = 24:
+		mn = 256
+	if mask = 23:
+		mn = 512
+
+#Main Body
 
 #Open the logs workbook
 logWorkbook = xlrd.open_workbook(logs.xlsx)
@@ -21,15 +45,34 @@ netR = 1
 #Define number of rows in Networks
 numNetRows = netSheet.nrows
 
-#define match variables 
-srcNetM = 0
-dstNetM = 0
-
 #Iterate through firewall logs
 while logR != numLogRows:
 
 	#Find network match for source address
-	while srcNetM = 0:
+	while netR != numNetRows:
+		srcIpLog = logSheet(logR,0)
+		srcNet = netSheet(netR,0)
+		srcMask = netSheet(netR,1)
+
+		#Grab the magic number for the subnet mask
+		srcMn = getMagicNumber(srcMask)
+
+
+
+		netR = netR + 1
+
+	#Start back at the top of the network spreadsheet	
+	netR = 1
+
+	#Find network match for destination address
+	while netR != numNetRows:
+		
+
+		netR = netR + 1
+
+
+
+
 	
 
 	logR = logR + 1
