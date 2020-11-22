6c1ded82c585df97fae1b3978368e8af04d65ce0
isocracy272 <crawl2720@hotmail.co.uk>
2020-11-22 20:15:43 +0000

    json

--- aci-sdk.py
+++ aci-sdk.py
@@ -1,9 +1,43 @@
 #imports
 import xlrd
+import json
 
 #functions
 
+#Format text from csv
+def formatText(text):
+	text = str(text)
+	text = text.replace("text:u","")
+	text = text.replace("'","")
+	text = text.lower()
+
+	return text
+
 #Create new BD
+def createBd(app,tier,environment,tenant,vrf,gateway):
+	bdName = environment + "-" + app + "-" + tier
+
+	bdJson = {
+	  "fvBD":{
+	    "attributes":{
+	      "dn":"uni/tn-" + tenant + "/BD-" + bdName + "",
+	      "mac":"00:22:BD:F8:19:FF",
+	      "name":"" + bdName + "",
+	      "rn":"BD-" + bdName + "",
+	      "status":"created"},
+	      "children":[{
+	        "fvRsCtx":{
+	          "attributes":{
+	            "tnFvCtxName":"" + vrf + "",
+	            "status":"created,modified"
+	          },
+	        "children":[]
+	      }
+	    }]
+	  }
+	}
+
+	return bdJson
 
 #Create new EPG
 
@@ -25,6 +59,35 @@ appsR = 1
 #Define number of rows in logs
 numAppsRows = appsSheet.nrows
 
+#create apps
+
+#Define variables
+app = appsSheet.cell(1,0)
+tier = appsSheet.cell(1,1)
+environment = appsSheet.cell(1,2)
+gateway = appsSheet.cell(1,3)
+tenant = appsSheet.cell(1,4)
+vrf = appsSheet.cell(1,5)
+
+#format app vars
+app = formatText(app)
+tier = formatText(tier)
+environment = formatText(environment)
+gateway = formatText(gateway)
+tenant = formatText(tenant)
+vrf = formatText(vrf)
+
+#Test function
+bdJson = createBd(app,tier,environment,tenant,vrf,gateway)
+
+#pretty printing and convert from Python Dict into JSON
+bdJson = json.dumps(bdJson, sort_keys=True, indent=4)
+
+#dump Json into file
+f = open("bd.json", "w")
+f.write(bdJson)
+f.close()
+
 #Open policy
 polWorkbook = xlrd.open_workbook("policy.xlsx")
 polSheet = polWorkbook.sheet_by_name('policy')
@@ -35,11 +98,18 @@ polR = 1
 #Define number of rows in logs
 numpolRows = polSheet.nrows
 
-app = appsSheet.cell(1,0)
-print app
+#create policy
 
-source = polSheet.cell(1,0)
-print source
+#Xlrd Tests
+#app = appsSheet.cell(1,0)
+#print app
+
+#source = polSheet.cell(1,0)
+#print source
+
+#push to apic
+
+
 
 
 
