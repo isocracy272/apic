3b71f7f3ef91cdaa3710a9365b0dce3d1ab1e97b
isocracy272 <crawl2720@hotmail.co.uk>
2020-11-22 12:10:13 +0000

    xlrd

--- aci-sdk.py
+++ aci-sdk.py
@@ -1,4 +1,4 @@
-#Imports
+#imports
 import xlrd
 
 #functions
@@ -14,3 +14,35 @@ import xlrd
 #Associate Contract with Provider
 
 #main body
+
+#Open apps
+appsWorkbook = xlrd.open_workbook("apps.xlsx")
+appsSheet = appsWorkbook.sheet_by_name('apps')
+
+#Start at the top of the logs
+appsR = 1
+
+#Define number of rows in logs
+numAppsRows = appsSheet.nrows
+
+#Open policy
+polWorkbook = xlrd.open_workbook("policy.xlsx")
+polSheet = polWorkbook.sheet_by_name('policy')
+
+#Start at the top of the logs
+polR = 1
+
+#Define number of rows in logs
+numpolRows = polSheet.nrows
+
+app = appsSheet.cell(1,0)
+print app
+
+source = polSheet.cell(1,0)
+print source
+
+
+
+
+
+
